d5d95c5dc9986624d3225c2e638fc0371edec2df
guilhermepaulozup <guilherme.paulo@zup.com.br>
2023-05-18 10:47:09 -0300

    adjusting the action inputs

--- src/download.js
+++ src/download.js
@@ -4,8 +4,8 @@ const core = require('@actions/core');
 const tc = require("@actions/tool-cache");
 
 module.exports = async function () {
-    // const version = core.getInput("horusec-version");
-    const version = "v2.8.0"
+    const version = core.getInput("horusec-version");
+    // const version = "v2.8.0"
     const url = `https://github.com/ZupIT/horusec/releases/download/${version}/horusec_linux_amd64`;
     const horusecPath = await tc.downloadTool(url);
     fs.chmodSync(horusecPath, 0o755);
