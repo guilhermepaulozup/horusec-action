ea986a7541a731d360740d43d50fcd6e568ce566
guilhermepaulozup <guilherme.paulo@zup.com.br>
2023-05-19 12:13:33 -0300

    debugging input types

--- dist/index.js
+++ dist/index.js
@@ -13504,16 +13504,50 @@ module.exports = async function () {
 
 
 const core = __nccwpck_require__(2186);
-const inputs = [
-    "analysis-timeout",
-    "certificate-path",
-    "enable-commit-author",
-    "enable-git-history",
-    "enable-owasp-dependency-check",
-    "enable-shellcheck",
-    "ignore-severity",
-    "ignore",
-];
+const inputs = {
+    "analysis-timeout": {
+        type: "number",
+        value: "",
+    },
+    "certificate-path": {
+        type: "string",
+        value: "",
+    },
+    "ignore-severity": {
+        type: "string",
+        value: "",
+    },
+    "ignore": {
+        type: "string",
+        value: "",
+    },
+    "enable-commit-author": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-git-history": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-owasp-dependency-check": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-shellcheck": {
+        type: "boolean",
+        value: "",
+    },
+};
+
+function parseInputType(input, value) {
+    if (input.type !== typeof value) {
+        
+    }
+
+    if (input.type === "boolean") return Boolean(value);
+    else if (input.type === "number") return Number(value);
+    else return String(value);
+}
 
 /**
     Get the action flags.
@@ -13522,9 +13556,8 @@ function getFlags() {
     const flags = [];
 
     // grabs all inputs based on "flags" array.
-    for (let input of inputs) {
-        const value = core.getInput(input);
-        core.info(`Type of ${input}: ${typeof value}`);
+    for (let input of Object.keys(inputs)) {
+        const value = parseInputType(input, core.getInput(input));
         if (value) flags.push(`--${input}=${value}`);
     }
 
--- src/flags.js
+++ src/flags.js
@@ -1,16 +1,50 @@
 'use strict';
 
 const core = require('@actions/core');
-const inputs = [
-    "analysis-timeout",
-    "certificate-path",
-    "enable-commit-author",
-    "enable-git-history",
-    "enable-owasp-dependency-check",
-    "enable-shellcheck",
-    "ignore-severity",
-    "ignore",
-];
+const inputs = {
+    "analysis-timeout": {
+        type: "number",
+        value: "",
+    },
+    "certificate-path": {
+        type: "string",
+        value: "",
+    },
+    "ignore-severity": {
+        type: "string",
+        value: "",
+    },
+    "ignore": {
+        type: "string",
+        value: "",
+    },
+    "enable-commit-author": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-git-history": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-owasp-dependency-check": {
+        type: "boolean",
+        value: "",
+    },
+    "enable-shellcheck": {
+        type: "boolean",
+        value: "",
+    },
+};
+
+function parseInputType(input, value) {
+    if (input.type !== typeof value) {
+        
+    }
+
+    if (input.type === "boolean") return Boolean(value);
+    else if (input.type === "number") return Number(value);
+    else return String(value);
+}
 
 /**
     Get the action flags.
@@ -19,9 +53,8 @@ function getFlags() {
     const flags = [];
 
     // grabs all inputs based on "flags" array.
-    for (let input of inputs) {
-        const value = core.getInput(input);
-        core.info(`Type of ${input}: ${typeof value}`);
+    for (let input of Object.keys(inputs)) {
+        const value = parseInputType(input, core.getInput(input));
         if (value) flags.push(`--${input}=${value}`);
     }
 
